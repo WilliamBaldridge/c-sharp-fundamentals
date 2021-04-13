e14fb5f07113df4eceb8e8e8ecada913782bae29
WilliamBaldridge <baldridge.william09@gmail.com>
2021-04-12 19:49:14 -0500

    Learned about structs vs. classes,   and value and reference types with their relation to stack and heap

--- beginners/Person.cs
+++ beginners/Person.cs
@@ -3,18 +3,18 @@
 
 namespace CSHarpFundamentals.beginners
 {
-   
-        public class Person
+
+    public class Person
+    {
+
+        public int Age;
+        public String firstName;
+        public String lastName;
+
+        public void Introduce()
         {
-            public String firstName;
-            public String lastName;
-
-            public void Introduce()
-            {
-                Console.WriteLine("My name is " + firstName + " " + lastName);
-            }
-
+            Console.WriteLine("My name is " + firstName + " " + lastName);
         }
-
+    }
 
 }
---
+++ beginners/ReferenceAndValueTypes.cs
@@ -0,0 +1,35 @@
+﻿//using System;
+
+
+//namespace CSHarpFundamentals.beginners
+//{
+//    class ReferenceAndValueTypes
+//    {
+
+//        public static void Main(string[] args)
+//        {
+
+//            var number = 1;
+//            Increment(number);
+//            Console.WriteLine(number);
+
+//            var person = new Person() { Age = 20 };
+//            MakeOld(person);
+//            Console.WriteLine(person.Age);
+
+//        }
+
+
+//        private static void Increment(int number)
+//        {
+//            number += 10;
+//        }
+
+//        public static void MakeOld(Person person)
+//        {
+//            person.Age += 10;
+//        }
+
+//    }
+
+//}
